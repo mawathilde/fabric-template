44f7cb4286783bf086f03500a2a68c56b73145cc
mawathilde <mawathilde@gmail.com>
2022-10-20 12:51:39 +0200

    Initial commit

---
+++ src/main/java/fr/mawathilde/template/TemplateMod.java
@@ -0,0 +1,16 @@
+package fr.mawathilde.template;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemplateMod implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
